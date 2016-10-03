61984ef1fbe67a523c89885eb48c90c85f2d8b62
Malinna Leach <malinnab@yahoo.com>
2016-10-03 16:09:03 +0100

    top-up function

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,11 +2,17 @@ require 'oystercard'
 
 describe Oystercard do
 
-
-  it {expect(subject.is_a?(Oystercard)).to eq true}
+  before do
+    @top_up_value = 5
+  end
 
   it "Expect new card to have a 0 balance" do
     expect(subject.balance).to eq 0
+  end
+
+  it "Should reflect top-up value in balance" do
+    subject.top_up(@top_up_value)
+    expect(subject.balance).to eq @top_up_value
   end
 
 end
