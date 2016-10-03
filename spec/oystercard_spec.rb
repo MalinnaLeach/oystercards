2ac23df43abc945e7cca85e973d5f81621f54da2
Chris Lawrence <chris.lawrence@gmx.com>
2016-10-03 15:37:11 +0100

    Added balance attribute to Oystercard, tests passing

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,12 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+
+
+  it {expect(subject.is_a?(Oystercard)).to eq true}
+
+  it "Expect new card to have a 0 balance" do
+    expect(subject.balance).to eq 0
+  end
+
 end
